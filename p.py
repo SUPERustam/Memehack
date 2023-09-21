27ff0a13c4dcf96c1d83370d49327a3c6ab04e58
Rustam Uzdenov <64551202+SUPERustam@users.noreply.github.com>
2023-09-21 11:40:47 +0000

    🧚 Write paddleocr functions, make new tests, optimize workplace

--- p.py
+++ p.py
@@ -1,11 +1,27 @@
+#!/home/codespace/.python/current/bin/python
 from paddleocr import PaddleOCR
-import os
-import sys
-import cv2
 
-ocr = PaddleOCR(use_angle_cls=True, lang='en')
-img_path = 'i.jpg'
 
-result = ocr.ocr(img_path, cls=True)
-for line in result:
-    print(line)
+def setup_paddleocr():
+    ocr_cyr = PaddleOCR(use_angle_cls=True, lang='cyrillic')
+    ocr_en = PaddleOCR(use_angle_cls=True, lang='en')
+    yield ocr_cyr, ocr_en
+
+
+def image2text(img_path: str, ocr_model: PaddleOCR) -> list:
+    result = ocr_model.ocr(img_path, cls=False)
+    return result[0]
+
+
+def main():
+    img_path = 'images/rus3_hard.jpg'
+    ocr_cyr, ocr_en = next(setup_paddleocr())
+
+    result = image2text(img_path, ocr_cyr)
+
+    with open('ans.txt', 'w') as t:
+        t.write(str(result))
+
+
+if "__main__" == __name__:
+    main()
