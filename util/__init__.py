635c015fa5278a74cc7ca890de9cd32f92bb30b6
SUPERustam <498rustam@gmail.com>
2023-10-22 11:41:06 +0300

    🐜 Add util – utility for work

---
+++ util/__init__.py
@@ -0,0 +1,19 @@
+from functools import wraps
+import time
+import string
+
+def timeit(func):
+    """ measure execution time of function"""
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        total_time = time.perf_counter() - start_time
+        print(
+            f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
+
+
+def normalization_text(text: string) -> string:
+    return text.translate(str.maketrans('', '', string.punctuation))
