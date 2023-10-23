b8952f872bda0129fc127d0f09b408ccd14ad77e
SUPERustam <498rustam@gmail.com>
2023-10-23 11:02:13 +0300

    Update upload_module

--- upload_module/ocr.py
+++ upload_module/ocr.py
@@ -10,14 +10,18 @@ def setup_paddleocr():
     ocr_en = PaddleOCR(use_angle_cls=True, lang='en')
     yield ocr_cyr, ocr_en
 
-def image2text(ocr_model: PaddleOCR, img_path: str) -> list:
+def image2text(ocr_model: PaddleOCR, img_path: str) -> str:
     result = ocr_model.ocr(img_path, cls=False)
-    return " ".join((t[1][0] for t in result[0]))
+    try:
+        a = " ".join((t[1][0] for t in result[0]))
+    except *:
+        print(a)
+    return a
+
 
 def main():
     img_path = 'ocr_images/eng1.png'
     ocr_cyr, ocr_en = next(setup_paddleocr())
-    print(util.normalization_text('sdfdsfsf.dff.'))
-
+    
 if "__main__" == __name__:
     main()
--- upload_module/process_images.py
+++ upload_module/process_images.py
@@ -1,24 +1,36 @@
 import subprocess
+from subprocess import Popen, PIPE
 import urllib.request
 import sys
-
+import psycopg2
 import ocr
+
 sys.path.append('.')
-import util
 from db import func_db as fdb
+import util
 
-
-# https://vk.com/album-206845783_00
 
 
 @util.timeit
-def parse_vk_album(link: str, *filename: (str | None)) -> None:
-    if filename:
-        f = open(filename, 'w')
-        subprocess.run(f"gallery-dl -g {link}".split(), stdout=f)
-        f.close()
-        return
-        
+def parse_vk_album(link: str, *filename):
+    # if filename:
+    #     # f = open(filename[0], 'w')
+    #     # subprocess.run(f"gallery-dl -g {link}".split(), stdout=f)
+    #     # f.close()
+    #     f = subprocess.run(args=f"gallery-dl -g {link}".split(),
+    #                           universal_newlines=True,
+    #                           stdout=subprocess.PIPE)
+
+    #     nmap_lines = f.stdout.splitlines()
+    #     print(nmap_lines)
+    #     return
+
+    with Popen(f"gallery-dl -g {link}".split(), stdout=PIPE, universal_newlines=True) as process:
+        for line in process.stdout:
+            l = line.rstrip()
+
+
+
 
 @util.timeit
 def to_vk_link(link: str) -> str:
@@ -26,18 +38,65 @@ def to_vk_link(link: str) -> str:
 
 
 @util.timeit
-def pipeline(filename: str):
-    with open(filename, 'r') as f:
-        imgs = f.readlines()
+def pipeline(link: str):
+    # connect to db
+    try:
+        conn = psycopg2.connect(
+            dbname="memehackdb",
+            user="artyom",
+            host="localhost",
+            port=5432
+        )
+    except psycopg2.Error as error:
+        print("I was unable to connect to the database MemeHackDB!\n"
+              "Error: {error}")
+        sys.exit(1)
     
+    # initialization
     ocr_cyr, ocr_en = next(ocr.setup_paddleocr())
-    for img in imgs[:2]:  # todo: remove slice
-        img = img[:-1] # string end up with '\n'
-        urllib.request.urlretrieve(img, 'upload_module/pipeline_image.jpg')
+    cur = conn.cursor()
 
-        text_cyr = util.normalization_text(ocr.image2text(ocr_cyr, 'upload_module/pipeline_image.jpg'))
-        text_en = util.normalization_text(ocr.image2text(ocr_en, 'upload_module/pipeline_image.jpg'))
+    # write links
+    length_of_links = []
+
+    with Popen(f"gallery-dl -g {link}".split(), stdout=PIPE, universal_newlines=True) as process:
+        stp = 0
+        for img in process.stdout:
+            stp += 1
+            if stp == 3:
+                break
+            img = img[:-1]  # string end up with '\n'
+            
+            length_of_links.append(img)
+
+            urllib.request.urlretrieve(img, 'upload_module/pipeline_image.jpg')
+
+            text_ru = util.normalization_text(ocr.image2text(
+                ocr_cyr, 'upload_module/pipeline_image.jpg'))
+            text_en = util.normalization_text(ocr.image2text(
+                ocr_en, 'upload_module/pipeline_image.jpg'))
+
+            fdb.insert_image(cur, vk=img)
+            conn.commit()
+            fdb.insert_text(cur, img_id= ,text_ru, text_en)
+        
+        
+
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 if "__main__" == __name__:
-    pipeline('upload_module/file.txt')
+    pipeline('https://vk.com/album-206845783_00')
+    print('finish')
+    # 1. max 35.4 mib, Start time: 2023-10-22 23:32:36.399000
+# End time: 2023-10-22 23:33:04.989000
+# Total number of allocations: 343764
+# Total number of frames seen: 519
+    # l = list(parse_vk_album('https://vk.com/album-206845783_00'))
+    # print('finish')
+
+    
+
+    # pipeline('upload_module/file.txt')
