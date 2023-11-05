f2be8a623586dbf19a9436ca2f31ee70c961629f
SUPERustam <498rustam@gmail.com>
2023-11-05 19:24:05 +0300

    Update db for latest

--- lab/create.sql
+++ lab/create.sql
@@ -3,10 +3,9 @@ CREATE EXTENSION pg_trgm;
 CREATE TABLE images(
   id SERIAL PRIMARY KEY,
   vk VARCHAR(255) DEFAULT '_',
-  vk_small VARCHAR(255) DEFAULT '_', 
   tg VARCHAR(255) DEFAULT '_',
   tg_small VARCHAR(255) DEFAULT '_',
-  source_vk VARCHAR(255) DEFAULT '_'
+  source_vk INTEGER NOT NULL
 );
 
 CREATE TABLE texts(
@@ -19,15 +18,4 @@ CREATE TABLE texts(
 CREATE TABLE users(
   id BIGINT PRIMARY KEY,
   lang CHAR(2)
-);
-
-CREATE TABLE actions(
-  id SERIAL PRIMARY KEY,
-  time TIMESTAMP NOT NULL,
-  user_id BIGINT NOT NULL,
-  img_id SERIAL,
-  action CHAR(3) NOT NULL,
-  detail JSON,
-  FOREIGN KEY (img_id) REFERENCES images(id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
+)
