cd2813c7b2410aaac8b7f433a30b678e39fbbdb5
SUPERustam <498rustam@gmail.com>
2023-11-20 20:09:39 +0300

    🔐 New way to store secret variables

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+
+VK_ACCESS_TOKEN = os.environ['VK_ACCESS_TOKEN']
+VK_SERVER_ACCESS_KEY = os.environ['VK_SERVER_ACCESS_KEY']
+
+TG_TOKEN = os.environ['TG_TOKEN']
+TG_IMG_STORAGE_ID = [os.environ['TG_IMG_STORAGE_ID_1'],
+                     os.environ['TG_IMG_STORAGE_ID_2'],
+                     os.environ['TG_IMG_STORAGE_ID_3']]
+AMPLITUDE_API_KEY =  os.environ['AMPLITUDE_API_KEY'],
+POSTGRES_SERVER_PASSWORD = os.environ['POSTGRES_SERVER_PASSWORD'] 
