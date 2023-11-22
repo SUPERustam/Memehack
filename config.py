35c29faa04926c83acbe0461dcf72239f4322e05
SUPERustam <498rustam@gmail.com>
2023-11-22 20:55:29 +0300

    Update Docker for tgbot

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ VK_ACCESS_TOKEN = os.environ['VK_ACCESS_TOKEN']
 VK_SERVER_ACCESS_KEY = os.environ['VK_SERVER_ACCESS_KEY']
 
 TG_TOKEN = os.environ['TG_TOKEN']
-TG_IMG_STORAGE_ID = [os.environ['TG_IMG_STORAGE_ID_1'],
-                     os.environ['TG_IMG_STORAGE_ID_2'],
-                     os.environ['TG_IMG_STORAGE_ID_3']]
+TG_IMG_STORAGE_ID = [int(os.environ['TG_IMG_STORAGE_ID_1']),
+                     int(os.environ['TG_IMG_STORAGE_ID_2']),
+                     int(os.environ['TG_IMG_STORAGE_ID_3'])]
 AMPLITUDE_API_KEY =  os.environ['AMPLITUDE_API_KEY'],
 POSTGRES_SERVER_PASSWORD = os.environ['POSTGRES_SERVER_PASSWORD'] 
--- tgbot.Dockerfile
+++ tgbot.Dockerfile
@@ -1,6 +1,13 @@
-FROM superustam/pre_memehack_tgbot
+FROM pypy:latest
 
-WORKDIR /app/
-COPY config.py config.py
-ENV PYTHONPATH="/app/"
+WORKDIR app/
+COPY --link util.py util.py
+COPY --link config.py config.py
+COPY --link tg_bot/ tg_bot/
+COPY --link db/ db/
+
+RUN pip install -r tg_bot/requirements_pypy.txt && rm -rf /root/.cache /tmp/pip-* /tmp/pip3-packages
+
+ENV PYTHONPATH="/app"
 ENTRYPOINT ["pypy3", "tg_bot/bot.py"]
+# ENTRYPOINT ["/bin/bash"]
